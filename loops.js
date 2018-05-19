61aa53ed648bc21a6b79403b5b57a12ea4daa9b7
Peter van Dijk <peter@petervandijk.net>
2018-05-19 18:37:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,13 @@
+function forLoop(array) {
+  for (i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`);
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+}
+
 function whileLoop(num) {
   while (num >= 0) {
     console.log(num);
